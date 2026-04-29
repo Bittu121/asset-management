16cf3ea6675331c3351e77a7a73ab609f80662eb
Bittu Kumar <bittukumar8713@gmail.com>
2026-04-29 17:59:54 +0530

    create support group api

--- src/app/components/header/Header.tsx
+++ src/app/components/header/Header.tsx
@@ -28,13 +28,10 @@
 //     return () => document.removeEventListener("mousedown", handleClickOutside);
 //   }, []);
 
- 
 //   const toggleTheme = () => {
 //     setDarkMode(!darkMode);
 //     setShowText(true);
 //   };
-
-
 
 //   const handleLogout = () => {
 //     localStorage.removeItem("token");
@@ -121,23 +118,17 @@
 
 "use client";
 
-import { useTheme } from "next-themes";
 import LightModeIcon from "@mui/icons-material/LightMode";
-import DarkModeIcon from "@mui/icons-material/DarkMode";
 import { Bell, LogOut, User } from "lucide-react";
 import { useState, useRef, useEffect } from "react";
 import { useRouter } from "next/navigation";
 
 function Header() {
-  const { theme, setTheme } = useTheme();
-
   const [mounted, setMounted] = useState(false);
   const [open, setOpen] = useState(false);
 
   const dropdownRef = useRef<HTMLDivElement>(null);
   const router = useRouter();
-
-   const darkMode = theme === "dark";
 
   useEffect(() => {
     setMounted(true);
@@ -157,10 +148,6 @@ function Header() {
     return () => document.removeEventListener("mousedown", handleClickOutside);
   }, []);
 
-  const toggleTheme = () => {
-    setTheme(darkMode ? "light" : "dark");
-  };
-
   const handleLogout = () => {
     localStorage.removeItem("token");
     router.push("/login");
@@ -168,12 +155,12 @@ function Header() {
 
   if (!mounted) {
     return (
-      <div className="w-full h-16 bg-white border-b border-gray-200 px-6 flex items-center justify-between" />
+      <div className="w-full h-16 bg-gray border-b border-gray-200 px-6 flex items-center justify-between" />
     );
   }
 
   return (
-    <div className="w-full h-16 bg-white dark:bg-gray-900 border-b border-gray-200 dark:border-gray-800 px-6 flex items-center justify-between">
+    <div className="w-full h-16 bg-gray-900 border-b border-gray-200 px-6 flex items-center justify-between">
       {/* LEFT */}
       <h1 className="text-sm font-semibold text-gray-800 dark:text-white tracking-wide">
         Asset Management
@@ -181,33 +168,9 @@ function Header() {
 
       {/* RIGHT */}
       <div className="flex items-center gap-3">
-        {/* THEME TOGGLE */}
-        <div className="relative flex items-center gap-2">
-          <span className="text-sm font-medium text-gray-600 dark:text-gray-300 min-w-18 text-right">
-            {darkMode ? "Dark Mode" : "Light Mode"}
-          </span>
-
-          <button
-            onClick={toggleTheme}
-            className="w-16 h-9 bg-gray-100 dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-full p-1 flex items-center transition-all duration-300"
-          >
-            <div
-              className={`w-7 h-7 rounded-full bg-white dark:bg-gray-950 shadow-sm flex items-center justify-center transition-all duration-300 ${
-                darkMode ? "translate-x-7" : "translate-x-0"
-              }`}
-            >
-              {darkMode ? (
-                <DarkModeIcon sx={{ fontSize: 16 }} />
-              ) : (
-                <LightModeIcon sx={{ fontSize: 16 }} />
-              )}
-            </div>
-          </button>
-        </div>
-
         {/* NOTIFICATION */}
-        <div className="relative p-2 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-800 cursor-pointer transition">
-          <Bell size={18} className="text-gray-600 dark:text-gray-300" />
+        <div className="relative p-2 rounded-lg hover:bg-gray-100 cursor-pointer transition">
+          <Bell size={18} className="text-gray-600" />
           <span className="absolute top-1 right-1 w-2 h-2 bg-red-500 rounded-full"></span>
         </div>
 
@@ -221,23 +184,23 @@ function Header() {
           />
 
           {open && (
-            <div className="absolute right-0 top-12 z-50 w-44 bg-white dark:bg-gray-900 border border-gray-200 dark:border-gray-700 rounded-xl shadow-lg overflow-hidden">
+            <div className="absolute right-0 top-12 z-50 w-44 bg-white border border-gray-200 rounded-xl shadow-lg overflow-hidden">
               <button
                 onClick={() => {
                   router.push("/profile");
                   setOpen(false);
                 }}
-                className="flex items-center gap-2 w-full px-4 py-3 text-sm text-gray-700 dark:text-gray-200 hover:bg-gray-50 dark:hover:bg-gray-800 transition"
+                className="flex items-center gap-2 w-full px-4 py-3 text-sm text-gray-700  hover:bg-gray-50 dark:hover:bg-gray-800 transition"
               >
                 <User size={16} />
                 Profile
               </button>
 
-              <div className="border-t border-gray-200 dark:border-gray-700" />
+              <div className="border-t border-gray-200 " />
 
               <button
                 onClick={handleLogout}
-                className="flex items-center gap-2 w-full px-4 py-3 text-sm text-red-600 hover:bg-red-50 dark:hover:bg-red-900/20 transition"
+                className="flex items-center gap-2 w-full px-4 py-3 text-sm text-red-600 hover:bg-red-50  transition"
               >
                 <LogOut size={16} />
                 Logout
